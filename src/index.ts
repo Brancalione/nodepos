05c24fc2b8cb38947915b86469ad0579e116f727
Brancalione <jeova.brancalione@gmail.com>
2025-05-28 22:22:35 -0300

    add knex

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { CategoryAllowed } from './Services/categoryService';
 import { Produto, ProdutoPermitido } from './Interface/types';
 // import Fastify, {FastifyInstance, FastifyServerOptions } from 'fastify'
 import { start } from './Server'
+
  
 async function validarCategoriasProds() {
     const produtosCaminho = path.join(__dirname, '../produtcts.json');
