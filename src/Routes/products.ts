5f888b50452e82278dea0ee7cc358a26c3c04d19
Brancalione <jeova.brancalione@gmail.com>
2025-06-14 11:43:22 -0300

    setimo commit autenticacao das rotas put post delete

--- src/Routes/products.ts
+++ src/Routes/products.ts
@@ -6,9 +6,9 @@ import { ProdutoPermitido } from "../Interface/types";
 export default async function productRoutes(server: FastifyInstance) {
     server.get('/products', productController.getAllProduts)
     server.get('/products/:id', productController.getProductById)
-    server.post <{ Body: ProdutoPermitido }>  ('/products', productController.postNewProduct)
-    server.put <{ Body: ProdutoPermitido }> ('/products/:id', productController.updateProduct)
-    server.put('/products/:id/image', productController.updateImageProduct)
-    server.delete('/products/:id', productController.deleteProductById)
+    server.post <{ Body: ProdutoPermitido }>  ('/products', { preHandler: [server.authenticate] } ,productController.postNewProduct)
+    server.put <{ Body: ProdutoPermitido }> ('/products/:id',{ preHandler: [server.authenticate] } ,productController.updateProduct)
+    server.put('/products/:id/image',{ preHandler: [server.authenticate] } ,productController.updateImageProduct)
+    server.delete('/products/:id', { preHandler: [server.authenticate] } , productController.deleteProductById)
     server.get('/login',  productController.getNewTokenJWT)
 } 
--- src/Server.ts
+++ src/Server.ts
@@ -1,6 +1,10 @@
-import Fastify, { FastifyInstance, FastifyServerOptions } from 'fastify'
+import Fastify, { FastifyInstance, FastifyReply, FastifyRequest, FastifyServerOptions } from 'fastify'
 import productRoutes from './Routes/products'
 import fastifyMultipart from '@fastify/multipart'
+import fastifyJwt from 'fastify-jwt';
+import dotenv from 'dotenv';
+
+dotenv.config(); 
 
 const server: FastifyInstance = Fastify({ logger: true })
 server.get('/', async (request, reply) => {
@@ -9,7 +13,27 @@ server.get('/', async (request, reply) => {
     }
 })
 
+
 server.register(fastifyMultipart)
+
+server.register(fastifyJwt, {
+  secret: process.env.JWT_SECRET as string,
+  sign: {
+    expiresIn: process.env.JWT_EXPIRES_IN || '24h',
+  }
+});
+
+// Middleware global de autenticação — pode ser usado nas rotas como `preHandler: [server.authenticate]`
+server.decorate("authenticate", async (request:FastifyRequest, reply:FastifyReply) => {
+  try {
+    await request.jwtVerify(); // Valida e preenche `request.user`
+  } catch (err) {
+    reply.code(401).send({ message: 'Token inválido ou ausente' });
+  }
+  
+});
+
+
 server.register(productRoutes)
 
 export const start = async () => {
